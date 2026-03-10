30b30632de1c594db61fdc009e244ec86736cf6d
Luiz Nunes <l.n.almeida.ti@gmail.com>
2026-03-09 23:41:46 -0300

    chore(cleanup): remove deprecated sales directory vestige

--- src/app/(admin)/admin/sales/register/[vehicleId]/_components/RegisterSaleForm.jsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import SaleForm from "../../../_components/SaleForm";
-
-export default function RegisterSaleForm({ vehicle, sellerId }) {
-    return (
-        <SaleForm vehicle={vehicle} sellerId={sellerId} />
-    );
-}
