6c2466b8fc9e2c73162a3c962a864940b91448ef
Luiz Nunes <l.n.almeida.ti@gmail.com>
2026-03-10 21:27:36 -0300

    fix(build): fix Prisma adapter destructuring in isolated action

--- src/actions/recovery.js
+++ src/actions/recovery.js
@@ -1,6 +1,6 @@
 "use server";
 
-import prisma from "@/lib/prisma";
+import { db as prisma } from "@/lib/prisma";
 import { auth, currentUser } from "@clerk/nextjs/server";
 import { revalidatePath } from "next/cache";
 
